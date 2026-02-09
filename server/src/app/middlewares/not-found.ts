3a6e34ee504e2e56555301ab322f89bc52a66554
hazzaz-am <hazzazabdulmannan@gmail.com>
2026-02-09 11:54:37 +0600

    clean server code

--- server/src/app/middlewares/not-found.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-import { StatusCodes } from "http-status-codes";
-
-export const notFoundHandler = (req: Request, res: Response) => {
-	res.status(StatusCodes.NOT_FOUND).json({
-		success: false,
-		message: `Cannot find ${req.originalUrl} on this server!`,
-	});
-};
--- server/src/app/modules/tour/tour.constants.ts
+++
@@ -1,2 +0,0 @@
-export const TOURSEARCHABLEFIELDS = ["title", "description", "location"]
-export const TOURTYPESEARCHABLEFIELDS = ["name"]
